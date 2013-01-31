80cf5bce3d9835be96a8854e0fec16f16fd52237
Oleg Lobachev <aironik@yandex-team.ru>
2013-02-01 00:04:38 +0400

    fix romb

--- Pacman/Game/Surfaces/Romb.mm
+++ Pacman/Game/Surfaces/Romb.mm
@@ -29,8 +29,9 @@ void Romb::generateVertexes() {
     GLKVector3 *const vertexDataSrc = (GLKVector3 *)malloc(sizeof(GLKVector3) * componentsCount * 2);
     GLKVector3 *vertexDataPtr = vertexDataSrc;
 
-    const GLKVector3 topVertex = GLKVector3Make(0.0f, -1.0f , 0.0f);
-    const GLKVector3 bottomVertex = GLKVector3Make(0.0f, 1.0f , 0.0f);
+    float height = 1.5f;
+    const GLKVector3 topVertex = GLKVector3Make(0.0f, -height , 0.0f);
+    const GLKVector3 bottomVertex = GLKVector3Make(0.0f, height , 0.0f);
     for (GLuint i = 0; i < sectionsCount; ++i) {
         float alpha0 = i * 2 * M_PI / sectionsCount;
         float alpha1 = (i + 1) * 2 * M_PI / sectionsCount;
@@ -38,26 +39,23 @@ void Romb::generateVertexes() {
         const GLKVector3 vertex0 = GLKVector3Make(cosf(alpha0), 0.0f, sinf(alpha0));
         const GLKVector3 vertex1 = GLKVector3Make(cosf(alpha1), 0.0f, sinf(alpha1));
 
+        GLKVector3 normal1 = GLKVector3CrossProduct(GLKVector3Subtract(vertex1, vertex0),
+                                                   GLKVector3Subtract(topVertex, vertex0));
         vertexDataPtr[0] = topVertex;
-        vertexDataPtr[1] = vertex0;
-        vertexDataPtr[2] = vertex1;
-
-        GLKVector3 normal1 = GLKVector3CrossProduct(GLKVector3Subtract(vertex0, vertex1),
-                                                   GLKVector3Subtract(topVertex, vertex1));
+        vertexDataPtr[1] = normal1;
+        vertexDataPtr[2] = vertex0;
         vertexDataPtr[3] = normal1;
-        vertexDataPtr[4] = normal1;
+        vertexDataPtr[4] = vertex1;
         vertexDataPtr[5] = normal1;
-
         vertexDataPtr += 6;
 
+        GLKVector3 normal2 = GLKVector3CrossProduct(GLKVector3Subtract(vertex0, vertex1),
+                                                   GLKVector3Subtract(bottomVertex, vertex1));
         vertexDataPtr[0] = bottomVertex;
-        vertexDataPtr[1] = vertex1;
-        vertexDataPtr[2] = vertex0;
-
-        GLKVector3 normal2 = GLKVector3CrossProduct(GLKVector3Subtract(vertex1, vertex0),
-                                                   GLKVector3Subtract(bottomVertex, vertex0));
+        vertexDataPtr[1] = normal2;
+        vertexDataPtr[2] = vertex1;
         vertexDataPtr[3] = normal2;
-        vertexDataPtr[4] = normal2;
+        vertexDataPtr[4] = vertex0;
         vertexDataPtr[5] = normal2;
         vertexDataPtr += 6;
     }
